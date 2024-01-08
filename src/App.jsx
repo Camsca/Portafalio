9ea5a9f7619aa1b0b83205d55e74affb32904da0
Camsca <camila_vass@hotmail.com>
2024-01-08 13:56:06 -0800

    intregated reac-icons to package,footer card with respective links and creatded file for styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './components/UI/Navbar/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/UI/Header/Header';
+import Footer from './components/UI/Footer/Footer';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
   <main >
         <Outlet />
       </main>
+<Footer/>
     </>
   )
 }
--- src/components/UI/Footer/Footer.jsx
+++ src/components/UI/Footer/Footer.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { FaGithub, FaTwitter } from 'react-icons/fa'; 
+import './footerStyles.css';
 
-const Footer= () => {
-    return (
-        <div>
-
-
-
-            
-        </div>
-    )
+function Footer() {
+  return (
+    <Card>
+      <Card.Footer className="text-muted d-flex justify-content-center footerStyles">
+        {/* Links to GitHub and Twitter */}
+        <a href="https://github.com/Camsca/" className="mx-3">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://twitter.com" className="mx-3">
+          <FaTwitter size={30} />
+        </a>
+      </Card.Footer>
+    </Card>
+  );
 }
+
 export default Footer;
