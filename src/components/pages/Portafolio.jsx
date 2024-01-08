91b9b5fbe0e72ad6b92424182fdc88525dad3c99
Camsca <camila_vass@hotmail.com>
2024-01-08 13:09:34 -0800

    objects with project details

--- src/components/pages/Portafolio.jsx
+++ src/components/pages/Portafolio.jsx
@@ -5,6 +5,41 @@ function Portafolio() {
   return (
    const [hovered, setHovered] = useState();
   );
+  //array of portafolio dta
+  const portfolioData = [
+    {
+      id: 1,
+      gif: 'src/assets/lava.gif',
+      title: 'Project 1',
+      description: 'Description for Project 1',
+      link: 'https://snappergee.github.io/bookworm/bestsellers.html'
+    },
+    {
+      id: 2,
+      image: 'src/assets/image3.jpeg',
+      title: 'Pocket Pal Note-Taking Application',
+      description: 'Pocket Pal is a simple web-based note-taking application that allows users to create, save, and manage their notes conveniently. It provides an easy-to-use interface for organizing and retrieving your notes.',
+      link: 'https://pocket-pal-0a3f48b51dea.herokuapp.com'
+    },
+    {
+      id: 3,
+      image: 'src/assets/blue.avif',
+      title: 'ElectroMarket Express',
+      description: 'ElectroMarket Express is a functional Express.js API that leverages Sequelize to interact with a MySQL database, providing a robust foundation for building e-commerce platforms in the electronics industry. This project allows businesses and consumers to conveniently engage in online buying and selling of electronic products.',
+       link: 'https://drive.google.com/file/d/1EmX3N2FLxmfvmIHB1toH1okgAZ-a0JWi/view'
+    },
+    {
+      id: 4,
+      image: 'src/assets/images 4.jpeg',
+      title: 'Code Connect: Social Network API',
+      description: 'Code Connect is an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Express.js is utilized for routing, alongside a MongoDB database, and the Mongoose ODM. Additionally, it makes use of the Express.js and Mongoose packages',
+      link: 'https://github.com/Camsca/CodeConnect'
+    },
+
+  ];
+  return(
+
+  )
 }
 
 export default Portafolio;
