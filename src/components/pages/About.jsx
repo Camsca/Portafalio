5197998b7f0c16b1d5d5f45469599cd35eb8d160
Camsca <camila_vass@hotmail.com>
2024-01-07 18:08:13 -0800

    about me card styling

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,22 +1,29 @@
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function About() {
     return (
         <Card className="bg-dark text-white">
-          <Card.Img src="holder.js/100px270" alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural lead-in
-              to additional content. This content is a little bit longer.
-            </Card.Text>
-            <Card.Text>Last updated 3 mins ago</Card.Text>
-          </Card.ImgOverlay>
-          <Card body>This is some text within a card body.</Card>
+            <Card.Body>
+                <Row>
+                    <Col xs={6}> {/* Establece el ancho de la columna para la imagen */}
+                        <Card.Img src="holder.js/100px270" alt="Card image" />
+                    </Col>
+                    <Col xs={6}> {/* Establece el ancho de la columna para el texto */}
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                        </Card.Text>
+                        <Card.Text>Last updated 3 mins ago</Card.Text>
+                    </Col>
+                </Row>
+            </Card.Body>
+            <Card.Body>
+                <Card.Text>This is some text within a card body.</Card.Text>
+            </Card.Body>
         </Card>
-    
-      );
-    }
-    
+    );
+}
 
 export default About;
