bf18faa984ea5973d898cd23ff33c1fb97d80181
Camsca <camila_vass@hotmail.com>
2024-01-08 22:17:34 -0800

    kinda finish styling

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,30 +2,25 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-
 function About() {
-    return (
-        <Card className="bg-dark text-white">
-           
-            <Card.Body>
-                <Row>
-                <Col xl={2}> {/* Ancho de la columna para el texto */}
-                        <Card.Img src="holder.js/100px270" alt="Card image" />
-                    </Col>
-                    <Col xl={8}> {/* Ancho de la columna para el texto */}
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                        </Card.Text>
-                        <Card.Text>Last updated 3 mins ago</Card.Text>
-                    </Col>
-                </Row>
-            </Card.Body>
-            <Card.Body>
-                <Card.Text>This is some text within a card body.</Card.Text>
-            </Card.Body>
-        </Card>
-    );
+  return (
+    <Card className="bg-dark text-white">
+      <Card.Body>
+        <Row>
+          <Col xs={4}> {/* Ancho de la columna para la imagen */}
+            <Card.Img src="src/assets/IMG_7608 Large.jpeg" alt="Card image" />
+          </Col>
+          <Col xl={8}> {/* Ancho de la columna para el texto */}
+            <Card.Title>About me</Card.Title>
+            <Card.Text>
+            Aspiring Full Stack Developer
+              Eager to Learn and Create: Thriving on the possibilities that coding offers, I'm immersing myself to gain the skills needed to craft impactful web applications. I've taken on the challenge of mastering technologies like JavaScript, Node, SQL, MongoDB, and React. My excitement lies in learning how these pieces fit together in the digital puzzle. 🌱 Future Goals and Beyond: As I progress, I'm eager to challenge myself with a single-page application using the MERN stack. This journey isn't just about acquiring skills; it's about transforming my passion for coding into a meaningful career.
+            </Card.Text>
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default About;
--- src/components/pages/Portafolio.jsx
+++ src/components/pages/Portafolio.jsx
@@ -10,28 +10,28 @@ function Portafolio() {
       id: 1,
       gif: 'src/assets/lava.gif',
       title: 'Project 1',
-      description: 'Description for Project 1',
+      description: 'Project 1 description.',
       link: 'https://snappergee.github.io/bookworm/bestsellers.html'
     },
     {
       id: 2,
       image: 'src/assets/image3.jpeg',
       title: 'Pocket Pal Note-Taking Application',
-      description: 'Pocket Pal is a simple web-based note-taking application that allows users to create, save, and manage their notes conveniently. It provides an easy-to-use interface for organizing and retrieving your notes.',
+      description: 'Pocket Pal is a simple web-based note-taking app for managing notes.',
       link: 'https://pocket-pal-0a3f48b51dea.herokuapp.com'
     },
     {
       id: 3,
       image: 'src/assets/blue.avif',
       title: 'ElectroMarket Express',
-      description: 'ElectroMarket Express is a functional Express.js API that leverages Sequelize to interact with a MySQL database, providing a robust foundation for building e-commerce platforms in the electronics industry. This project allows businesses and consumers to conveniently engage in online buying and selling of electronic products.',
-       link: 'https://drive.google.com/file/d/1EmX3N2FLxmfvmIHB1toH1okgAZ-a0JWi/view'
+      description: 'ElectroMarket Express: A functional Express.js API for e-commerce.',
+      link: 'https://drive.google.com/file/d/1EmX3N2FLxmfvmIHB1toH1okgAZ-a0JWi/view'
     },
     {
       id: 4,
       image: 'src/assets/images 4.jpeg',
       title: 'Code Connect: Social Network API',
-      description: 'Code Connect is an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list. Express.js is utilized for routing, alongside a MongoDB database, and the Mongoose ODM. Additionally, it makes use of the Express.js and Mongoose packages',
+      description: 'Code Connect: An API for a social network app using Express.js and MongoDB.',
       link: 'https://github.com/Camsca/CodeConnect'
     },
 
@@ -51,7 +51,7 @@ function Portafolio() {
                   position: 'relative',
                   overflow: 'hidden'
                 }}
-              >/  {/* Conditionally rendering project GIF or image */}
+              > {/* Conditionally rendering project GIF or image */}
                 {project.gif ? (
                   <Card.Img
                     variant="top"
@@ -76,7 +76,7 @@ function Portafolio() {
                   />
                 )}
                  {/* Text displayed on hover */}
-                <div
+                 <div
                   className="hover-text"
                   style={{
                     position: 'absolute',
@@ -84,11 +84,13 @@ function Portafolio() {
                     left: '50%',
                     transform: 'translate(-50%, -50%)',
                     opacity: hovered === project.id ? 1 : 0,
-                    transition: 'opacity 0.3s ease-in-out'
+                    transition: 'opacity 0.3s ease-in-out',
+                    textAlign: 'center',
+                    color: 'orange', // Change text color
                   }}
                 >
-                  <span>{project.title}</span>
-                  <p>{project.description}</p>
+                  <span style={{ fontSize: '18px', fontWeight: 'bold' }}>{project.title}</span>
+                  <p style={{ fontSize: '12px' }}>{project.description}</p>
                 </div>
               </Card>
             </Link>
